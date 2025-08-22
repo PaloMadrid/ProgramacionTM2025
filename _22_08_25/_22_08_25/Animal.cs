790b34cdcdfa780ac2579c321e2ba7d080a18aac
PaloMadrid <paloma.madrid@bue.edu.ar>
2025-08-22 11:16:58 -0300

    practica de clases

---
+++ _22_08_25/_22_08_25/Animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_08_25
+{
+     class Animal
+    {
+        //declaracion de atributos
+        private string nombre;
+        private int cantidad_de_patas;
+        private string tipo;
+
+        //constructor
+        public Animal(string nombreDelBicho, int cantidad_patas, string tipo)
+        {
+            this.Nombre = nombreDelBicho;
+            this.Cantidad_de_patas = cantidad_patas;
+            this.Tipo = tipo;
+        }
+
+        //metodos get set
+        public string Nombre { get ; set ; }
+        public int Cantidad_de_patas { get ; set ; }
+        public string Tipo { get ; set ; }
+
+        //metodo de accion
+
+        public void ComerVoid()
+        {
+            Console.WriteLine( Nombre + " esta comiendo.");
+        }
+
+        public void MostrarDatos()
+        {
+            //no puedo usar este metodo para ingresar informacion, solo para mostrarla
+            Console.WriteLine("La mascota ingresada se llama "+Nombre+ " es de tipo "+Tipo+" y tiene "+Cantidad_de_patas+" patas");
+        }
+    }
+}
---
+++ _22_08_25/_22_08_25/Testeo_animales.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _22_08_25;
+
+Console.WriteLine("Hello, World!");
+
+string nombre_animal1 = "";
+string tipo_animal1;
+int cantidad_patas_animal1;
+
+Console.WriteLine("Ingrese el nombre de la mascota:");
+nombre_animal1 = Console.ReadLine();
+
+Console.WriteLine("Ingrese el tipo de mascota:");
+tipo_animal1 = Console.ReadLine();
+
+Console.WriteLine("Ingrese la cantidad de patas:");
+cantidad_patas_animal1 = int.Parse(Console.ReadLine());
+
+Animal animal1 = new Animal(nombre_animal1, cantidad_patas_animal1, tipo_animal1);
+Animal animal2 = new Animal("Doggy",4,"Perro");
+
+animal1.MostrarDatos();
+
+animal2.MostrarDatos();
+
+animal2.ComerVoid();
