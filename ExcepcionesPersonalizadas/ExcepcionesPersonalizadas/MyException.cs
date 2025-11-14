5cc6494d9370e9bcaf9d868dd5fc0991a87d2d63
PaloMadrid <163356132+PaloMadrid@users.noreply.github.com>
2025-11-14 10:45:33 -0300

    Add files via upload

---
+++ ExcepcionesPersonalizadas/ExcepcionesPersonalizadas/MyException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    internal class MyException : Exception
+    {
+        Object objetoError;
+        public MyException(string mensaje, Object objetoError) : base(mensaje)
+        {
+            this.objetoError = objetoError;
+        }
+
+        public Object Error
+        {
+            get { return objetoError; }
+        }
+
+
+    }
+}
---
+++ ExcepcionesPersonalizadas/ExcepcionesPersonalizadas/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+
+namespace Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Object> list = new List<Object> { 1, 2, 6, 4, "PERRO", 5 };
+            List<int> enteros = new List<int>();
+
+            try
+            {
+                CargarLista(list, enteros);
+            }
+            catch (MyException ex)
+            {
+                Console.WriteLine($"Intento agregar el elemento: {ex.Error.ToString()}\n"+ex.Message );
+            }
+            finally
+            {
+
+                foreach (int i in enteros)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+
+
+
+
+
+        }
+
+        static void CargarLista(List<Object> lista, List<int> enteros)
+        {
+
+            foreach (Object elemento in lista)
+            {
+                if (elemento is int)
+                {
+                    enteros.Add((int)elemento);//unboxing
+                }
+                else
+                {
+                    throw new MyException("Imposible agregar un elemento a la lista.", elemento);
+                }
+            }
+
+        }
+
+
+
+
+
+
+
+    }
+
+}
+
